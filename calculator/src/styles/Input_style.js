a74a8b8c53db70aa4410de1288309f4cad399026
yoonza <zoony1110@sch.ac.kr>
2024-04-18 21:54:12 +0900

    입력창과 드롭다운 박스 생성

--- calculator/src/styles/Input_style.js
+++ calculator/src/styles/Input_style.js
@@ -1,24 +1,11 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: flex;
-  flex: 1;
-  justify-content: space-between;
-  width: 250px;
+
 `;
 
 export const Input = styled.input`
-  display: block;
-  padding: 5px 0px 5px 10px;
-  margin-right: 5px;
-  height: 30px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
 
-  &:focus {
-    border: 1px solid #000;
-    outline: none;
-  }
 `;
 
 export const SelectBox = styled.select``;
